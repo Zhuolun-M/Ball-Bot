6e604823b0de7f7cd1f560fc6f2f647791049937
Zhuolun-M <81269397+Zhuolun-M@users.noreply.github.com>
2023-06-29 11:43:48 -0500

    added the robot class code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ballbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ballbot.java
@@ -1,11 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class ballbot {
-    DcMotor Motor1;
-    DcMotor Motor2;
-    DcMotor Motor3;
+    public DcMotor Motor1;
+    public DcMotor Motor2;
+    public DcMotor Motor3;
 
+    HardwareMap map;
+
+    public void init(HardwareMap m){
+        map = m;
+        Motor1 = m.get(DcMotor.class, "m1");
+        Motor2 = m.get(DcMotor.class, "m2");
+        Motor3 = m.get(DcMotor.class, "m3");
+
+        Motor3.setDirection(DcMotorSimple.Direction.FORWARD);
+        Motor2.setDirection(DcMotorSimple.Direction.FORWARD);
+        Motor1.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        Motor1.setPower(0.0);
+        Motor2.setPower(0.0);
+        Motor3.setPower(0.0);
+
+        Motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
 
 }
