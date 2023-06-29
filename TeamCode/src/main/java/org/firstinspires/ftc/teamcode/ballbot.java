e15722c8814ddbed84eab24a489f01bf66abcda0
Zhuolun-M <81269397+Zhuolun-M@users.noreply.github.com>
2023-06-29 11:35:16 -0500

    test2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ballbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ballbot.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class ballbot {
+    DcMotor Motor1;
+    DcMotor Motor2;
+    DcMotor Motor3;
+
 
 }
