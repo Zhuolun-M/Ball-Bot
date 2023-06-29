41727cfe1a30b13a626eff3aac8040705a0ab7a1
Zhuolun-M <81269397+Zhuolun-M@users.noreply.github.com>
2023-06-29 12:23:00 -0500

    fixed stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ballbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ballbot.java
@@ -3,12 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
 
 public class ballbot {
     //Declared 3 motors to be used
     public DcMotor Motor1;
     public DcMotor Motor2;
     public DcMotor Motor3;
+    public IMU imu = null;
 
     HardwareMap map;
 
@@ -18,10 +20,12 @@ public class ballbot {
         Motor1 = m.get(DcMotor.class, "m1");
         Motor2 = m.get(DcMotor.class, "m2");
         Motor3 = m.get(DcMotor.class, "m3");
+        imu = m.get(IMU.class,"imu");
         //Sets initial direction of the motors to forward
         Motor3.setDirection(DcMotorSimple.Direction.FORWARD);
         Motor2.setDirection(DcMotorSimple.Direction.FORWARD);
         Motor1.setDirection(DcMotorSimple.Direction.FORWARD);
+
         //Initial power is zero, absence of motion
         Motor1.setPower(0.0);
         Motor2.setPower(0.0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readIMU.java
@@ -4,20 +4,20 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp(name = "LiveReadingOfIMU", group = "Sensor")
 public class readIMU extends LinearOpMode {
-    IMU imu = null;
+    ballbot robot = new ballbot();
     // We name an IMU class IMU which we will then initialize later.
 
-    @Override public void runOpMode() throws InterruptedException {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
         // This calls the IMU of that is located on the expansion/control hub which should be
         // named IMU.
-        imu = hardwareMap.get(IMU.class,"imu");
         //Here we declare two  variables
         // that the orientation of the hub is facing upward as shown by the logo and that the direction of the
         // USB port is facing forward.
@@ -25,13 +25,13 @@ public class readIMU extends LinearOpMode {
         RevHubOrientationOnRobot.UsbFacingDirection UD = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
         RevHubOrientationOnRobot RO = new RevHubOrientationOnRobot(LD, UD);
 
-        imu.initialize(new IMU.Parameters(RO));
+        robot.imu.initialize(new IMU.Parameters(RO));
         // Here we initialize the variable imu by using the RO variable
 
         waitForStart();
         // We will wait for the drivestation to be activated
 
-        imu.resetYaw();
+        robot.imu.resetYaw();
         //Each time we start our robot we will reset the YAW to make sure that where the robot starts
         // that location becomes the new origin
 
@@ -40,13 +40,20 @@ public class readIMU extends LinearOpMode {
             telemetry.addData("Hub orientation", "Logo=%s   USB=%s\n ", LD, UD);
             // This telemetry tells the drive the orientation the Hub is supposed to be in.
 
-            YawPitchRollAngles ori = imu.getRobotYawPitchRollAngles();
+            YawPitchRollAngles ori = robot.imu.getRobotYawPitchRollAngles();
             // The YawPitchRollAngles class is extended from the IMU class in order to contain the readings of
             // Yaw, Pitch, Roll, and Angles of the IMU sensor in the hub.
             // We will create a variable called ori to access those readings.
-            AngularVelocity ang = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
+            AngularVelocity ang = robot.imu.getRobotAngularVelocity(AngleUnit.DEGREES);
             // This line will instead contain the Angular Velocities of the change in Yaw, Pitch, and Roll,
             // instead of the values themselves
+
+            if(gamepad1.a){
+                robot.Motor1.setPower(1.0);
+            }
+            else{
+                robot.Motor1.setPower(0.0);
+            }
 
             telemetry.addData("YAW (Z)", "%.2f Deg.", ori.getYaw(AngleUnit.DEGREES));
             telemetry.addData("PITCH (X)","%.2f Deg.", ori.getPitch(AngleUnit.DEGREES));
@@ -58,8 +65,8 @@ public class readIMU extends LinearOpMode {
             // We will add Data to the telemetry on the values of the Yaw, Pitch, Roll, and their respective angular velocites
             // We will also update all the telemtry at the same time for the driver
 
-            sleep(500);
-            // Updates the code every 0.5 seconds
+            sleep(250);
+            // Updates the code every 0.25 seconds
         }
     }
 }
