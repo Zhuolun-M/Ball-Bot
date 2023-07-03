7636a78f6669ca6b39c2ac9295ecbf7aa2d47a61
Zhuolun-M <81269397+Zhuolun-M@users.noreply.github.com>
2023-07-03 10:17:32 -0500

    PIDcontrol1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ballbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ballbot.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class ballbot {
     //Declared 3 motors to be used
@@ -12,23 +13,28 @@ public class ballbot {
     public DcMotor Motor3;
     public IMU imu = null;
 
+    ElapsedTime timer = new ElapsedTime();
+
     //Motor 1 PID values
     public double Int_Sum_1 = 0;
     public double Kp_1 = 0;
     public double Ki_1 = 0;
     public double Kd_1 = 0;
+    public double prev_error_1 = 0;
 
     //Motor 2 PID values
     public double Int_Sum_2 = 0;
     public double Kp_2 = 0;
     public double Ki_2 = 0;
     public double Kd_2 = 0;
+    public double prev_error_1 = 0;
 
     //Motor 3 PID values
     public double Int_Sum_3 = 0;
     public double Kp_3 = 0;
     public double Ki_3 = 0;
     public double Kd_3 = 0;
+    public double prev_error_1 = 0;
 
 
 
@@ -57,7 +63,15 @@ public class ballbot {
 
 
     }
-    public double PIDctrl (){
+    public double PIDctrl_1 (double p, double r){
+        double error = Math.abs(p) + Math.abs(r);
+        double p_to_r = Math.abs(error - Math.abs(p))/Math.abs(error-Math.abs(r));
+
+        Int_Sum_1 += error*timer.seconds();
+        double deriv = (error - prev_error_1) / timer.seconds();
+        prev_error_1 = error;
+
+        double motor_power = Kp_1 * error + Ki_1 * Int_Sum_1 + Kd_1 * deriv;
 
     }
 }
