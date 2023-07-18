56b2b237c17af7aed299c63ce28459dc09fe7eb4
Zhuolun-M <81269397+Zhuolun-M@users.noreply.github.com>
2023-07-17 21:12:08 -0500

    Added "code" for backup bot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Balancebot.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Balancebot {
+
+    public DcMotor LeftMotor;
+    public DcMotor RightMotor;
+    public IMU imu;
+    ElapsedTime time1 = new ElapsedTime();
+
+    public double Kp = 0.0;
+    public double Ki = 0.0;
+    public double Kd = 0.0;
+    public double Int_sum = 0.0;
+    public double prev_error = 0.0;
+
+    HardwareMap maps;
+
+    public void init(HardwareMap m){
+        maps = m;
+        LeftMotor = m.get(DcMotor.class, "lf");
+        RightMotor = m.get(DcMotor.class,"rg");
+        imu = m.get(IMU.class, "imu");
+
+        LeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        LeftMotor.setPower(0.0);
+        RightMotor.setPower(0.0);
+
+        LeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+    public double PID_Ctrl(double tp, double p){
+        double error = (tp - p);
+
+        Int_sum += error*time1.seconds();
+        double deriv = (error - prev_error) / time1.seconds();
+        prev_error = error;
+        time1.reset();
+
+        double motor_power = Kp * error + Ki * Int_sum + Kd * deriv;
+        return motor_power;
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BalancebotControl.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+@Autonomous(name = "2-Wheel Balance")
+public class BalancebotControl extends LinearOpMode{
+    Balancebot robot = new Balancebot();
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        robot.init(hardwareMap);
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+        RevHubOrientationOnRobot.LogoFacingDirection LD = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection UD = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        RevHubOrientationOnRobot RO = new RevHubOrientationOnRobot(LD, UD);
+        robot.imu.initialize(new IMU.Parameters(RO));
+
+        robot.time1.reset();
+        double power = 0.0;
+
+        robot.imu.resetYaw();
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            telemetry.addData("Hub orientation", "Logo=%s   USB=%s\n ", LD, UD);
+
+            YawPitchRollAngles ori = robot.imu.getRobotYawPitchRollAngles();
+            AngularVelocity ang = robot.imu.getRobotAngularVelocity(AngleUnit.RADIANS);
+
+            if (gamepad1.a){
+                robot.LeftMotor.setPower(0.0);
+                robot.RightMotor.setPower(0.0);
+            }
+            else {
+                power = robot.PID_Ctrl(0.0, ori.getPitch(AngleUnit.RADIANS));
+                robot.LeftMotor.setPower(power);
+                robot.RightMotor.setPower(power);
+            }
+
+            telemetry.addData("YAW (Z)", "%.2f Rad.", ori.getYaw(AngleUnit.RADIANS));
+            telemetry.addData("PITCH (X)","%.2f Rad.", ori.getPitch(AngleUnit.RADIANS));
+            telemetry.addData("ROLL (Y)", "%.2f Rad.", ori.getRoll(AngleUnit.RADIANS));
+            telemetry.addData("YAW (Z) Velocity", "%.2f Rad/Sec", ang.zRotationRate);
+            telemetry.addData("PITCH (X) Velocity","%.2f Rad/Sec", ang.xRotationRate);
+            telemetry.addData("ROLL (Y) Velocity", "%.2f Rad/Sec", ang.yRotationRate);
+            telemetry.addData("Motor Power", "%.2f", power);
+            telemetry.update();
+
+            sleep(20);
+        }
+    }
+}
